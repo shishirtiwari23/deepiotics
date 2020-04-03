ded7cc13903cb9c821684019712eb1057deadfec
shishirtiwari23 <shishir456tiwari@gmail.com>
2020-04-03 20:31:57 +0530

    First Commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function openTool(evt, toolName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(toolName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  document.getElementById("defaultOpen").click();
